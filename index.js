62de9c248b17421f8d889f3ab27dc48559ae376b
paullsshin <paullsshin@hotmail.com>
2023-03-17 17:41:03 -0700

    working on adding logic for the other functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const Department = require("./lib/Department");
+// const Department = require("./lib/Department");
 // const Employee = require("./lib/Employee");
 // const Role = require("./lib/Role");
 const db = require("./config/connection");
@@ -20,9 +20,8 @@ function initializer()  {
                 "Add an Employee",
                 "View Roles",
                 "Add a Role",
-                "View Managers",
-                "Update a Manager",
-                "View Budget of Each Department",
+                "Update an Employee",
+                "Terminate Program"
                 
             ]
             // message: "Select One of the Options",
@@ -32,31 +31,25 @@ function initializer()  {
         let answer = response.mainMenu;
         switch (answer) {
             case "View Departments":
-                getDepartments();
+                viewDepartments();
                 break;
             case "Add a Department":
-                createDepartment();
+                addDepartment();
                 break;
             case "View Employees":
-                getEmployees();
+                viewEmployees();
                 break;
             case "Add an Employee":
-                createEmployee();
+                addEmployee();
                 break;
             case "View Roles":
-                getRoles();
+                viewRoles();
                 break;
             case "Add a Role":
-                createRole();
+                addRole();
                 break;
-            case "View Managers":
-                getManagers();
-                break;
-            case "Update a Manger":
-                updateManager();
-                break;
-            case "View Budget":
-                getBudget();
+            case "Update an Employee":
+                updateEmployee();
                 break;
             default:
                 terminateProgram();
@@ -66,7 +59,7 @@ function initializer()  {
     })
 };
 
-const getDepartments = async () => {
+const viewDepartments = async () => {
   let query = "Select * FROM employee_db.department"
   db.query(query, (err, res) => {
     if (err) throw(err);
@@ -75,7 +68,7 @@ const getDepartments = async () => {
   })
 }
 
-const createDepartment = async () => {
+const addDepartment = async () => {
     inquirer
     .prompt([
         {
@@ -85,11 +78,17 @@ const createDepartment = async () => {
         },
     ])
     .then (async (response) => {
-        let query = "Select"
+        let query = "INSERT department(name) VALUES(?)"
+        db.query(query, response.createDepartment, (err,res) => {
+            if (err) throw (err);
+            console.log(res),
+            console.log("Department added");
+        })
+    
     })
 }
 
-const getEmployees = async () => {
+const viewEmployees = async () => {
     let query = "Select employee.id, employee.first_name, employee.last_name, role.title, department.name as department, role.salary, concat(manager.first_name, ' ', manager.last_name) as manager from employee left join role on employee.role_id=role.id left join department on role.department_id=department.id left join employee manager on employee.manager_id=manager.id"
     db.query(query, (err, res) => {
         if (err) throw(err);
@@ -97,4 +96,100 @@ const getEmployees = async () => {
         initializer();
       })
 }
+
+const addEmployee = async () => {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "employeeName",
+            message: "What is the first name of your new employee?"
+        },
+        {
+            type: "input",
+            name: "employeeLastName",
+            message: "What is the last name of your employee?"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "What is the ID of your employee?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the employee's manager ID?"
+        },
+    ])
+    .then (async(response) => {
+        let query = "INSERT INTO employee_db.employee(first_name, last_name, role_id, manager_id) VALUES(?)"
+        db.query(query, response.employeeName, response.employeeLastName, response.roleId, response.managerId, (err,res) => {
+            if (err) throw (err);
+            console.log(res,"Employee Added!");
+            initializer();
+        })
+    })
+}
+
+const viewRoles = async () => {
+    let query = "Select * FROM employee_db.role"
+    db.query(query, (err, res) => {
+      if (err) throw(err);
+      console.table(res)
+      initializer();
+    })
+  }
+
+  const addRole = async () => {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What role will you be adding?",
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary for this role?",
+        },
+        {
+            type: "input",
+            name: "departmentId",
+            message: "What department ID will this role be assigned to?",
+        },
+    ])
+    .then (async (response) => {
+        let query = "INSERT INTO employee_db.role(title, salary, department_id) VALUES(?)"
+        db.query(query, response.title, response.salary, response.department_id, (err, res) => {
+            if(err) throw(err);
+            console.log(res, "Role added!")
+        })
+    })
+  }
+
+  const updateEmployee = async () => {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "chooseEmployee",
+            message: "Which employee would you like to update?"
+        },
+        {
+            type: "input",
+            name: "updateEmployee",
+            message: "Which role will you be assigning to the employee?"
+        },
+    ])
+    .then (async (response) => {
+        let query = "Select employee.first_name, employee.last_name, role.title"
+    db.query(query, response.first_name, response.last_name, response.role_id, (err, res) => {
+        if (err) throw(err);
+        console.table(res)
+        initializer();
+      })
+    })
+  }
+
 initializer();
